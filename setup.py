3d3da7cee476b22413fcef6cf5a8ec6a88189a1e
Daniel Brakke <brakphoto@gmail.com>
2018-06-15 12:56:59 -0500

    first steps to publishing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='timelapse_eq',
+      version='0.1',
+      description='A Python package for creating smooth EV changes in timelapse photography',
+      url='http://github.com/brakkum/timelapse_eq',
+      author='Daniel Brakke',
+      author_email='brakphoto@gmail.com',
+      license='MIT',
+      packages=['timelapse_eq'],
+      zip_safe=False)
