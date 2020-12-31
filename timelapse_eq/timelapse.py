5f57eca77947ec3361094f722bf77721ab20a381
Daniel Brakke <daniel.brakke@fjorgedigital.com>
2020-12-30 18:46:58 -0600

    change array generation

--- timelapse_eq/timelapse.py
+++ timelapse_eq/timelapse.py
@@ -63,7 +63,7 @@ class TimeLapse:
         return change_points
 
     def determine_necessary_exposure_changes(self):
-        necessary_exposure_changes = [0] * len(self.photos)
+        necessary_exposure_changes = [0 for _ in range(len(self.photos))]
 
         change_points = iter(self.change_points)
         current_change_point = next(change_points)
