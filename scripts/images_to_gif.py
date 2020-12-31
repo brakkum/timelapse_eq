36cb40bfdd56f850f048827d90c76d02541237e2
Daniel Brakke <daniel.brakke@fjorgedigital.com>
2020-12-30 21:08:09 -0600

    fix case

--- scripts/images_to_gif.py
+++ scripts/images_to_gif.py
@@ -7,5 +7,5 @@ files = os.listdir(path)
 images = []
 for pic in sorted(files):
     if os.path.splitext(pic)[1] == ".jpeg":
-        images.append(imageio.imread(F"{path}/{pic}"))
+        images.append(imageio.imread(f"{path}/{pic}"))
 imageio.mimsave(f"{path}/timelapse.gif", images, fps=12)
